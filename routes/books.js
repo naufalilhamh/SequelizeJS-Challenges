5aa0289b935a8e5954eeb6bf748a76ddd3c911dd
naufal <naufalilham09@gmail.com>
2020-02-13 14:37:03 +0700

    Commit Mau Di kumpul

--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,5 @@
 var model = require("../models/index");
+const { check, validationResult } = require("express-validator");
 module.exports = function(app) {
   /* GET todo listing. */
   app.get("/books", function(req, res, next) {
@@ -18,80 +19,128 @@ module.exports = function(app) {
       );
   });
   /* POST todo. */
-  app.post("/books", function(req, res) {
-    const {
-      title,
-      author,
-      published_date,
-      pages,
-      language,
-      publisher_id
-    } = req.body;
-    model.Books.create({
-      title: title,
-      author: author,
-      published_date: published_date,
-      pages: pages,
-      language: language,
-      publisher_id: publisher_id
-    })
-      .then(books =>
-        res.status(201).json({
-          error: false,
-          data: books,
-          message: "Buku Baru Sudah Ditambahkan!"
-        })
-      )
-      .catch(error =>
-        res.json({
-          error: true,
-          data: [],
-          error: error
-        })
-      );
-  });
-  /* update todo. */
-  app.put("/books/:id", function(req, res, next) {
-    const books_id = req.params.id;
-    const {
-      title,
-      author,
-      published_date,
-      pages,
-      language,
-      publisher_id
-    } = req.body;
-    model.Books.update(
-      {
+  app.post(
+    "/books",
+    [
+      check("title")
+        .isLength({ min: 5 })
+        .withMessage("Minimal 5 Huruf !"),
+      check("author")
+        .isString()
+        .isLength({ min: 5 })
+        .withMessage("Harus String dan Minimal 5 Huruf"),
+      check("language")
+        .isString()
+        .isLength({ min: 5 })
+        .withMessage("Harus String dan Minimal 5 Huruf"),
+      check("publisher_id")
+        .isString()
+        .withMessage("Harus String dan Minimal 5 Huruf")
+        .isLength({ min: 5 })
+    ],
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      const {
+        title,
+        author,
+        published_date,
+        pages,
+        language,
+        publisher_id
+      } = req.body;
+      model.Books.create({
         title: title,
         author: author,
         published_date: published_date,
         pages: pages,
         language: language,
         publisher_id: publisher_id
-      },
-      {
-        where: {
-          id: books_id
-        }
+      })
+        .then(books =>
+          res.status(201).json({
+            error: false,
+            data: books,
+            message: "Buku Baru Sudah Ditambahkan!"
+          })
+        )
+        .catch(error =>
+          res.json({
+            error: true,
+            data: [],
+            error: error
+          })
+        );
+    }
+  );
+  /* update todo. */
+  app.put(
+    "/books/:id",
+    [
+      check("title")
+        .isLength({ min: 5 })
+        .withMessage("Minimal 5 Huruf !"),
+      check("author")
+        .isString()
+        .isLength({ min: 5 })
+        .withMessage("Harus String dan Minimal 5 Huruf"),
+      check("language")
+        .isString()
+        .isLength({ min: 5 })
+        .withMessage("Harus String dan Minimal 5 Huruf"),
+      check("publisher_id")
+        .isString()
+        .withMessage("Harus String dan Minimal 5 Huruf")
+        .isLength({ min: 5 })
+    ],
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
       }
-    )
-      .then(books =>
-        res.json({
-          error: false,
-          message: "Buku Sudah DiUpdate!"
-        })
+      const books_id = req.params.id;
+      const {
+        title,
+        author,
+        published_date,
+        pages,
+        language,
+        publisher_id
+      } = req.body;
+      model.Books.update(
+        {
+          title: title,
+          author: author,
+          published_date: published_date,
+          pages: pages,
+          language: language,
+          publisher_id: publisher_id
+        },
+        {
+          where: {
+            id: books_id
+          }
+        }
       )
-      .catch(error =>
-        res.json({
-          error: true,
-          error: error
-        })
-      );
-  });
+        .then(books =>
+          res.json({
+            error: false,
+            message: "Buku Sudah DiUpdate!"
+          })
+        )
+        .catch(error =>
+          res.json({
+            error: true,
+            error: error
+          })
+        );
+    }
+  );
   /* GET todo listing. */
   /* Delete todo. */
-  app.delete("books/:id", function(req, res, next) {
+  app.delete("/books/:id", function(req, res, next) {
     const books_id = req.params.id;
     model.Books.destroy({
       where: {
@@ -101,7 +150,7 @@ module.exports = function(app) {
       .then(status =>
         res.json({
           error: false,
-          message: "todo has been delete."
+          message: "Book has been delete."
         })
       )
       .catch(error =>
